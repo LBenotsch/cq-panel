ec6f8d85673aa6c3f8f16eb8c2fa785d7c4b7b25
Lucas Benotsch <lucasbenotsch@gmail.com>
2018-08-24 01:04:37 -0500

    error handling/cleanup

--- js/index.js
+++ js/index.js
@@ -15,11 +15,31 @@ $(document).ready(function () {
         });
     });
 
+    function changeCoinError() {
+        toastr.options = {
+            "closeButton": true,
+            "debug": false,
+            "newestOnTop": false,
+            "progressBar": false,
+            "positionClass": "toast-bottom-left",
+            "preventDuplicates": false,
+            "onclick": null,
+            "showDuration": "300",
+            "hideDuration": "1000",
+            "timeOut": "5000",
+            "extendedTimeOut": "1000",
+            "showEasing": "swing",
+            "hideEasing": "linear",
+            "showMethod": "fadeIn",
+            "hideMethod": "fadeOut"
+        }
+        toastr.error("Chaning coins failed!");
+    }
+
     var coin;
     $("#change_coin").click(function () {
         document.getElementById('change_coin').disabled = true;
         document.getElementById('options').disabled = true;
-
 
         coin = document.getElementById("options").textContent;
         if (coin.trim() == "Select a coin to mine" || coin == "" || coin == null) {
@@ -47,7 +67,7 @@ $(document).ready(function () {
                 jQuery("#change_coin").html('Save changes');
                 document.getElementById('options').disabled = false;
             }, 300000); //5 mins
-            $.post(NODE_BACKEND_URL + "change_coin", { coin: coin }, function (data) {
+            $.post("" + "change_coin", { coin: coin }, function (data) {
                 if (data === 'done') {
                     toastr.options = {
                         "closeButton": true,
@@ -67,7 +87,14 @@ $(document).ready(function () {
                         "hideMethod": "fadeOut"
                     }
                     toastr.success("Coin saved. Changing rigs to: " + coin + ". Changes will take effect in about 5 minutes.");
+                } else {
+                    jQuery("#change_coin").html('Error');
+                    changeCoinError()
                 }
+            })
+            .fail(function(response) {
+                jQuery("#change_coin").html('Error');
+                changeCoinError()
             });
         }
     });
@@ -78,54 +105,53 @@ $.get(NODE_BACKEND_URL + 'which_coin', function (data) {
     document.getElementById("options").textContent = data;
 });
 
-//Pulls json data
-$.getJSON(PANEL_JSON_URL, function (data, status) {
-    //var preobj = JSON.parse(text.responseText);
-    var obj = data.rigs;
+    //Pulls json data
+    $.getJSON(PANEL_JSON_URL, function (data, status) {
+        var rigs = data.rigs;
 
-    //Fill data for top blocks
-    document.getElementById("active-miners").innerHTML = data.alive_rigs + "/" + data.total_rigs;
-    document.getElementById("active-gpus").innerHTML = data.alive_gpus;
-    document.getElementById("total-hash").innerHTML = Math.round(data.total_hash);
-    document.getElementById("avg-gpu-temp").innerHTML = Math.round(data.avg_temp) + "c";
+        //Fill data for top blocks
+        document.getElementById("active-miners").innerHTML = data.alive_rigs + "/" + data.total_rigs;
+        document.getElementById("active-gpus").innerHTML = data.alive_gpus;
+        document.getElementById("total-hash").innerHTML = Math.round(data.total_hash);
+        document.getElementById("avg-gpu-temp").innerHTML = Math.round(data.avg_temp) + "c";
 
-    //Fill miner details table
-    var k = '<tbody>';
-    for (i = 0; i < obj.length; i++) {
-        switch (obj[i].pool) {
-            case 'us1.ethermine.org:4444':
-                coin = 'ETH';
-                break;
-            case 'us1-etc.ethermine.org:4444':
-                coin = 'ETC';
-                break;
-            case 'us-east.pirlpool.eu:8004':
-                coin = 'PIRL';
-                break;
-            case 'musicoin.nomnom.technology:4444':
-                coin = 'MUSIC';
-                break;
-            case 'lb.geo.ellapool.eu:8004':
-                coin = 'ELLA';
-                break;
-            case 'us.expmine.pro:9009':
-                coin = 'EXP';
-                break;
-            case 'us.ubiqpool.io:8008':
-                coin = 'UBQ';
-                break;
-            default:
-                coin = 'Unknown';
+        //Fill miner details table
+        var k = '<tbody>';
+        for (i = 0; i < rigs.length; i++) {
+            switch (rigs[i].pool) {
+                case 'us1.ethermine.org:4444':
+                    coin = 'ETH';
+                    break;
+                case 'us1-etc.ethermine.org:4444':
+                    coin = 'ETC';
+                    break;
+                case 'us-east.pirlpool.eu:8004':
+                    coin = 'PIRL';
+                    break;
+                case 'musicoin.nomnom.technology:4444':
+                    coin = 'MUSIC';
+                    break;
+                case 'lb.geo.ellapool.eu:8004':
+                    coin = 'ELLA';
+                    break;
+                case 'us.expmine.pro:9009':
+                    coin = 'EXP';
+                    break;
+                case 'us.ubiqpool.io:8008':
+                    coin = 'UBQ';
+                    break;
+                default:
+                    coin = 'Unknown';
+            }
+            k += '<tr>';
+            k += '<td>' + rigs[i].rack_loc + '</td>';
+            k += '<td>' + rigs[i].ip + '</td>';
+            k += '<td>' + rigs[i].gpus + '</td>';
+            k += '<td>' + rigs[i].hash + '</td>';
+            k += '<td>' + coin + '</td>';
+            k += '</tr>';
         }
-        k += '<tr>';
-        k += '<td>' + obj[i].rack_loc + '</td>';
-        k += '<td>' + obj[i].ip + '</td>';
-        k += '<td>' + obj[i].gpus + '</td>';
-        k += '<td>' + obj[i].hash + '</td>';
-        k += '<td>' + coin + '</td>';
-        k += '</tr>';
-    }
-    k += '</tbody>';
-    document.getElementById('tableData').innerHTML = k;
-})
-    .fail(function () { alert('getJSON request failed! For ' + url); });
+        k += '</tbody>';
+        document.getElementById('tableData').innerHTML = k;
+    })
+        //.fail(function () { alert('getJSON request failed! For ' + PANEL_JSON_URL); });
